a8ba6d5a09152a80f2e9f438fde3b4b33adbc7e9
RoohitD <66574477+RoohitD@users.noreply.github.com>
2024-12-23 14:52:44 -0500

    Initial Commit

---
+++ budjet_tracker/budjet_trackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  budjet_trackerApp.swift
+//  budjet_tracker
+//
+//  Created by Rohit Deshmukh on 12/23/24.
+//
+
+import SwiftUI
+
+@main
+struct budjet_trackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ budjet_trackerTests/budjet_trackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  budjet_trackerTests.swift
+//  budjet_trackerTests
+//
+//  Created by Rohit Deshmukh on 12/23/24.
+//
+
+import Testing
+@testable import budjet_tracker
+
+struct budjet_trackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
