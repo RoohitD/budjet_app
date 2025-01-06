5dc05737f64aae0abc6092cc5df02ba7c262fc03
RoohitD <66574477+RoohitD@users.noreply.github.com>
2025-01-06 17:27:36 -0500

    Updated ListView for Categorization
    - Transactions are categorized into section based on their selected categories

--- budjet_tracker/HomePage/ExpenseListView.swift
+++ budjet_tracker/HomePage/ExpenseListView.swift
@@ -11,23 +11,34 @@ struct ExpenseListView: View {
     @Environment(\.managedObjectContext) var moc
     @FetchRequest(sortDescriptors: [NSSortDescriptor(keyPath: \ExpenseEntity.date, ascending: false)])
     var expenses: FetchedResults<ExpenseEntity>
+    var groupedExpenses: [String: [ExpenseEntity]] {
+        Dictionary(grouping: expenses) { expense in
+            expense.expenseTocategory?.name ?? "No Category"
+        }
+    }
     
     var body: some View {
         // List of expenses
         List {
-            ForEach(expenses, id: \.self) { expense in
-                NavigationLink(value: expense) {
-                    ExpenseListItem(expense: expense)
+            ForEach(groupedExpenses.keys.sorted(), id: \.self) { category in
+                Section(header: Text(category)) {
+                    ForEach(groupedExpenses[category] ?? [], id: \.self) { expense in
+                        NavigationLink(value: expense) {
+                            ExpenseListItem(expense: expense)
+                        }
+                    }
+                    .onDelete{ offsets in
+                        delete(expense: groupedExpenses[category] ?? [], at: offsets)
+                    }
                 }
             }
-            .onDelete(perform: delete)
         }
         .navigationDestination(for: ExpenseEntity.self) { expense in
             ExpenseDetailPage(expense: expense)
         }
     }
     
-    func delete(at offsets: IndexSet) {
+    func delete(expense: [ExpenseEntity], at offsets: IndexSet) {
         for index in offsets {
             let expense = expenses[index]
             moc.delete(expense)
